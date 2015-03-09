cc32497c4e3dd3729a96e39e1c3568bea419734d
juandipg <jdp5@rice.edu>
2015-03-09 11:14:40 -0500

    Initialized linked list of free pages, compiles and runs successfully

--- yalnix.c
+++ yalnix.c
@@ -1,6 +1,8 @@
 #include <comp421/hardware.h>
 #include <comp421/yalnix.h>
 
+#define NULL 0;
+
 void TrapKernel(ExceptionStackFrame *frame);
 void TrapClock(ExceptionStackFrame *frame);
 void TrapIllegal(ExceptionStackFrame *frame);
@@ -10,6 +12,14 @@ void TrapTtyReceive(ExceptionStackFrame *frame);
 void TrapTtyTransmit(ExceptionStackFrame *frame);
 
 void * vector_table[7];
+
+typedef struct FreePage FreePage;
+
+struct FreePage {
+    FreePage *next;
+};
+
+struct FreePage * firstFreePage = NULL;
 
 void
 TrapKernel(ExceptionStackFrame *frame)
@@ -89,6 +99,39 @@ KernelStart(ExceptionStackFrame *frame,
     TracePrintf(0, "Saved addr of ivt to REG_VECTOR_BASE register\n");
     
     // Step 2: Build list of available page tables
+    FreePage * page;
+    
+    // two loops
+    // first one, from PMEM_BASE (0) to KERNEL_STACK_BASE
+    // this is everything in region 0 except for the kernel stack
+    // TODO: is KERNEL_STACK_BASE addressable?
+    for (page = (FreePage *) PMEM_BASE + MEM_INVALID_SIZE; 
+            page < (FreePage *) KERNEL_STACK_BASE; 
+            page += PAGESIZE) 
+    {
+        TracePrintf(1, "Adding address %p to list\n", page);
+        page->next = firstFreePage;
+        firstFreePage = page;
+    }
+    
+    TracePrintf(1, "Got through region 0\n");
+    TracePrintf(1, "Kernel stack base is at %p\n", (void *) KERNEL_STACK_BASE);
+    TracePrintf(1, "We have %d bytes of physical memory\n", pmem_size);
+    TracePrintf(1, "Size of void * is: %d\n", sizeof(void *));
+    TracePrintf(1, "Size of FreePage * is: %d\n", sizeof(FreePage *));
+    TracePrintf(1, "memory ends at %p\n", ((FreePage *) PMEM_BASE) + pmem_size / sizeof(void *));
+    
+    // second one, from orig_brk to pmem_size
+    // pmem_size is in bytes, which conveniently is the smallest
+    // addressable unit of memory
+    for (page = (FreePage *) orig_brk; 
+            page < ((FreePage *) PMEM_BASE) + pmem_size / sizeof(void *); 
+            page += PAGESIZE) 
+    {
+        TracePrintf(1, "Adding address %p to list\n", page);
+        page->next = firstFreePage;
+        firstFreePage = page;
+    }
     
     // Step 3: Build page tables for Region 1 and Region 0
     
